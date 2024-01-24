7441a442414ddc291d534b55cc8cc02f76e7b65c
Jordan Duran <jordanduranm@gmail.com>
2024-01-24 13:35:01 -0500

    Chapter 12

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -16,7 +16,6 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
     params.set('page', pageNumber.toString());
     return `${pathname}?${params.toString()}`;
   };
-  // NOTE: comment in this code when you get to this point in the course
 
   const allPages = generatePagination(currentPage, totalPages);
 
